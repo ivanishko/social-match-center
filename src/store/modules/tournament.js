e75164a02d8387011998e916ab982adc114f2ccb
Aleksandr Ivanishko <ivanishco@mail.ru>
2020-07-03 11:44:10 +0300

    create Tourmanent c.2

--- src/store/modules/tournament.js
+++ src/store/modules/tournament.js
@@ -7,14 +7,36 @@ export default {
   state: {
     tournament: {}
   },
-  mutations: {},
-
-  getters: {
-    item(state) {
-      return state.match;
+  mutations: {
+    initTournament(state, data){
+      state.tournament = data;
     },
   },
 
-  actions: {},
+  getters: {
+    item(state) {
+      return state.tournament;
+    },
+    name(state) {
+      return state.tournament.name_translate;
+    },
+    local(state) {
+      return state.tournament.loc_translate;
+    },
+    region(state) {
+      return state.tournament.reg_translate;
+    },
+  },
+
+  actions: {
+    loadTournament(store,id)  {
+      axios.get(`http://localhost:3000/tournaments/` + id)
+        .then(response => {
+          //console.log(response)
+          console.log(response.data);
+          store.commit('initTournament',response.data)
+        })
+    },
+  }
 
 }
